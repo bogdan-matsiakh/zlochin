4124b3c074333320910feb1ece86f9a5722569d6
kostjerry <kostjerry@gmail.com>
2013-04-14 17:47:39 +0300

    123

--- zlochin/static/js/parser.js
+++ zlochin/static/js/parser.js
@@ -1,5 +1,41 @@
 parser = (function () {
-    var _items = [],
+    var _items = {
+				"Зареєстровані злочини": {
+					"усього": {
+						"торік": {
+							styleID: 1,
+							templID: 1,
+							colName: "col1\x3e\x3e1"
+						},
+						"поточний рік": {
+							styleID: 1,
+							templID: 1,
+							colName: "col2\x3e\x3e1"
+						},
+						"динаміка, %": {
+							styleID: 1,
+							templID: 1,
+							colName: "col3\x3e\x3e1"
+						}
+					},
+					"тяжких та особливо тяжких": {
+						"торік": {
+							styleID: 1,
+							templID: 1,
+							colName: "col1\x3e\x3e1"
+						},
+						"поточний рік": {
+							styleID: 1,
+							templID: 1,
+							colName: "col2\x3e\x3e1"
+						},
+						"динаміка, %": {
+							styleID: 1,
+							templID: 1,
+							colName: "col3\x3e\x3e1"
+						}
+					}
+				},
         _parse = function (data) {
             var elems = data.feed.entry;
                 
