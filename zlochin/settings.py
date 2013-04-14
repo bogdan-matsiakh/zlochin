c3280fe2936b18e932ce58f0b6bb2b906b5f72c4
bogdan-matsiakh <bmatsiakh@gmail.com>
2013-04-14 15:03:30 +0300

    site render

--- zlochin/settings.py
+++ zlochin/settings.py
@@ -10,17 +10,16 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 # Parse database configuration from $DATABASE_URL
-'''
+
 import dj_database_url
 DATABASES = {
     'default':dj_database_url.config()
 }
-'''
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 MANAGERS = ADMINS
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -32,7 +31,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
-
+'''
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
