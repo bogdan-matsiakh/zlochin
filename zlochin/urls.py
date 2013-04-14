3bb76d77c0169196f141cee5f8b10723085b7e67
bogdan-matsiakh <bmatsiakh@gmail.com>
2013-04-14 19:58:54 +0300

    merged

--- zlochin/urls.py
+++ zlochin/urls.py
@@ -9,7 +9,7 @@ from main import views
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.simple import redirect_to
+#from django.views.generic.simple import redirect_to
 
 
 urlpatterns = patterns('main.views',
