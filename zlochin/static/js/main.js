469277d5d19c901ead8fd357cd93bc92234e0f53
kostjerry <kostjerry@gmail.com>
2013-04-15 02:31:10 +0300

    I go sleep

--- zlochin/static/js/main.js
+++ zlochin/static/js/main.js
@@ -1,28 +1,12 @@
 function loadList(data) {
 	parser.parse(data);
-	$( "#accordion" ).accordion({
-    	heightStyle: "fill"
+	$("#accordion").accordion({
+    	heightStyle: "content"
     });
-    $( "#accordion-resizer" ).resizable({
-		minHeight: 140,
-		minWidth: 200,
-		resize: function() {
-			$("#accordion").accordion({
-				heightStyle: 'content'
-			});
-		}
-    });
-    $('input').button();
 }
 
 // <=== ENTRY POINT :)
 $(function() {
 	$.getScript('https://spreadsheets.google.com/feeds/list/0As9SVzApMBjodHNIVTRTM09nUFFtZzV5aXhDQzZTbnc/od6/public/values?alt=json-in-script&callback=loadList');
-	
 	parser.init();
-	mapWorker.show({
-		colName: 'col210',
-		styleID: 3,
-		templID: 4
-	});
 });
