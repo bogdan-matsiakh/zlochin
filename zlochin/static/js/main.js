1541f9b8c81735c0ff80601936101c8a5fe75a93
bogdan-matsiakh <bmatsiakh@gmail.com>
2013-04-15 02:33:19 +0300

    discuss widget

--- zlochin/static/js/main.js
+++ zlochin/static/js/main.js
@@ -19,6 +19,16 @@ function loadList(data) {
 $(function() {
 	$.getScript('https://spreadsheets.google.com/feeds/list/0As9SVzApMBjodHNIVTRTM09nUFFtZzV5aXhDQzZTbnc/od6/public/values?alt=json-in-script&callback=loadList');
 	
+	$("#toggle_discus").click(function () {
+	    if ($(document.body).hasClass('show-discus')) {
+    	    $(this).html('Показати коментарі');
+    	    $(document.body).removeClass('show-discus');
+	    } else {
+	        $(this).html('Приховати коментарі');
+	        $(document.body).addClass('show-discus');
+	    }
+	})
+	
 	parser.init();
 	mapWorker.show({
 		colName: 'col210',
