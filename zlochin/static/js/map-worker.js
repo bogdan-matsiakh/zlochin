1ab8f5bea13348937e30ec4aed1f66169645c154
kostjerry <kostjerry@gmail.com>
2013-04-14 23:20:30 +0300

    Many changes performed

--- zlochin/static/js/map-worker.js
+++ zlochin/static/js/map-worker.js
@@ -2,6 +2,7 @@ mapWorker = (function () {
     var _townCenter = new google.maps.LatLng(46.918844877014486, 30.76445803124999),
     	_map = null,
     	_layer = null,
+    	_logoOverlay = null,
         _show = function (data) {
             _map = new google.maps.Map(document.getElementById('map'), {
 				center: _townCenter,
@@ -14,7 +15,7 @@ mapWorker = (function () {
 				heatmap: { enabled: false },
 				query: {
 					select: data.colName,
-					from: "1w0ufexIfgcSjm21d9EM2qICIa6jsyRM4ihHPE6o",
+					from: "1JLNhoP4rNdnGHk6UTGeVAqkivHdfSfckO-PaN8I",
 					where: ""
 				},
 				options: {
@@ -22,6 +23,20 @@ mapWorker = (function () {
 					templateId: data.templID
 				}
 			});
+			
+			_showLogo();
+        },
+        _showLogo = function(){
+        	var div = document.createElement('div'),
+        		img = document.createElement('img');
+			_map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(div);
+			div.style.border = 'none';
+			div.style.borderWidth = '0px';
+			div.style.position = 'absolute';
+			img.src = '../static/images/logo.png';
+			img.style.width = '200';
+			img.style.height = '101';
+			div.appendChild(img);
         };
     return {
         show : function (data) {
